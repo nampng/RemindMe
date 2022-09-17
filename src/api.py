260c35dda9a24822b595387ff76ae75eb906abf9
Nam <nampn344@gmail.com>
2022-09-17 13:29:07 -0500

    more api stuff

--- src/api.py
+++ src/api.py
@@ -8,14 +8,26 @@ import json
 class RoutineData(BaseModel):
     name: str
     day: int
-    target_time: time
+    target_time: time | None
     description: str = ""
+
+    def __iter__(self):
+        return (self.name, self.day, self.target_time, self.description)
+
+class Response(BaseModel):
+    msg: str
+    data: str | None
 
 app = FastAPI()
 scheduler = Scheduler()
 
 # https://stackoverflow.com/questions/11875770/how-to-overcome-datetime-datetime-not-json-serializable
 class TimeEncoder(json.JSONEncoder):
+    """
+    This is to convert time to str in json.dumps(). Probably won't need this since I'll just do
+    time.isoformat() and pass into json.dumps(), but maybe I'll need it later on for something else.
+    If not, this will be banished to the useless_files folder.
+    """
     def default(self, o):
         if isinstance(o, time):
             return o.isoformat()
@@ -23,32 +35,46 @@ class TimeEncoder(json.JSONEncoder):
         return json.JSONEncoder.default(self, o)
 
 @app.get("/")
-def root():
+def root() -> Response:
     """
     This returns a list of routines that have remind_me == True
     """
-    return "Hello, world"
+    reminders = scheduler.get_reminders()
+    return Response(msg=f"{len(reminders)} reminders retrieved.", data=json.dumps(reminders))
 
 @app.post("/add")
-def add_routine(data: RoutineData):
-    pass
+def add_routine(data: RoutineData) -> Response:
+    name, day, target_time, description = data
+    scheduler.add(Routine(
+        name=name, 
+        day=day, 
+        target_time=time.fromisoformat(target_time), 
+        description=description
+        ))
+    return Response(msg=f"Routine '{name}' added.")
 
 @app.post("/remove")
-def remove_routine(data: RoutineData):
-    pass
+def remove_routine(data: RoutineData) -> Response:
+    name, day, *_ = data
+    scheduler.remove(name=name, day=day)
+    return Response(msg=f"Routine '{name}' removed.")
 
 @app.post("/update")
-def update_routine(data: RoutineData):
-    pass
+def update_routine(data: RoutineData) -> Response:
+    name, day, target_time, description = data
+    scheduler.update(
+        name=name, 
+        day=day, 
+        target_time=time.fromisoformat(target_time), 
+        description=description)
+    return Response(msg=f"Routine '{name}' updated.")
 
-def clear_reminder(data: RoutineData):
-    pass
+def clear_reminder(data: RoutineData) -> Response:
+    name, day, *_ = data
+    scheduler.clear_reminder(name=name, day=day)
+    return Response(msg=f"Cleared reminder for {name}.")
 
 
 if __name__ == "__main__":
-    t = time(hour=1)
-    j = json.dumps(obj=t, cls=TimeEncoder)
-    j = json.loads(j)
-    t2 = time.fromisoformat(j)
-    print(t)
-    print(t2)
+    from datetime import datetime
+    print(datetime.now().time())
